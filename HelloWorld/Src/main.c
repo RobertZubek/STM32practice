3a9743a0f252d2e952c6af5e59c8c25aa79a80e1
Robert Zubek <robert.zubek.ofc@gmail.com>
2024-03-04 10:33:11 +0100

    .

--- HelloWorld/Src/main.c
+++ HelloWorld/Src/main.c
@@ -16,7 +16,7 @@
  ******************************************************************************
  */
 
-#include <stdint.h>
+//#include <stdint.h>
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
@@ -26,7 +26,14 @@
 
 int main(void)
 {
-    /* Loop forever */
-	printf("Hello world\n");
-	for(;;);
+
+
+
+
+
+	while(1){
+		printf("Hello world\n");
+	}
+
+
 }
